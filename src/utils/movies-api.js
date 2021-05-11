6b164673549afa7b8e84d5c28aec02e78a61bec2
Austin Summerlin <summerlin.austin@gmail.com>
2021-05-11 15:31:17 -0700

    add delete and update

--- src/utils/movies-api.js
+++ src/utils/movies-api.js
@@ -16,3 +16,13 @@ export async function addMovie(movie) {
   const response = await (await request.post(URL)).statusType('json').send(movie);
   return response.body.id;
 }
+
+export async function deleteMovie(id) {
+  const response = await request.delete(`${URL}/${id}`);
+  return response.body;
+}
+
+export async function updateMovie(movie) {
+  const response = await (await request.put(`${URL}/${movie.id}`)).statusType('json').send(movie);
+  return response.body;
+}
